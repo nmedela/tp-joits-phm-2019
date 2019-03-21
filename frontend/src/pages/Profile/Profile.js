af01a3e6c52f20edfa2d00d90afcb235593f4065
Guusy <guusygonzalo@gmail.com>
2019-03-21 20:54:05 -0300

    profile layout

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -5,40 +5,108 @@ import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import "./Profile.scss";
+import ProfileService from "../../services/ProfileService";
 
-const Profile = () => (
-  <div>
-    <Card>
-      <CardContent>
-        <div className="profile">
-          <div className="profile__side">
-            <div> Usuario John Code</div>
-            <div>
-              <TextField label="Edad" />
-            </div>
-            <div>
-              <div>Amigos</div>
-              <div>Tabla de amigos</div>
-            </div>
-          </div>
-          <div lassName="profile__side">
-            <div>Saldo : $1500</div>
-            <div>
-              <TextField label="Cargar Saldo" /> <Button>Cargar</Button>
-            </div>
-            <div>
-              <div>Pelis vistas</div>
-              <div>Lista de peliculas </div>
-            </div>
-          </div>
-        </div>
-      </CardContent>
-      <CardActions>
-        <Button>Buscar amigos</Button>
-        <Button>Aceptar</Button>
-        <Button>Cancelar</Button>
-      </CardActions>
-    </Card>
-  </div>
-);
+class Profile extends Component {
+  state = {
+    user: null,
+    moneyToCharge: null
+  };
+  async componentDidMount() {
+    const user = await ProfileService.getUserData();
+    this.setState({
+      user
+    });
+  }
+  chargeMoney = async () => {
+    try {
+      await ProfileService.chargeMoney(this.state.moneyToCharge);
+      this.setState({
+        moneyToCharge: ""
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  acepptModifies = async () => {
+    try {
+      await ProfileService.modifyUser(this.state.user);
+    } catch (e) {
+      console.log("error", e);
+    }
+  };
+  goToFriendsPage = () => {
+    this.props.history.push("/profile/friends");
+  };
+  render() {
+    const { user, moneyToCharge } = this.state;
+    return (
+      <div>
+        <Card>
+          <CardContent style={{ display: "flex", justifyContent: "center" }}>
+            {!!user ? (
+              <div className="profile">
+                <div className="profile__side">
+                  <div> Usuario {user.name}</div>
+                  <div>
+                    <TextField
+                      label="Edad"
+                      value={user.age}
+                      type="number"
+                      onChange={e =>
+                        this.setState({
+                          user: {
+                            ...this.state.user,
+                            age: e.target.value
+                          }
+                        })
+                      }
+                    />
+                  </div>
+                  <div>
+                    <div>Amigos</div>
+                    <div>Tabla de amigos</div>
+                  </div>
+                </div>
+                <div lassName="profile__side">
+                  <div>Saldo : ${user.money}</div>
+                  <div>
+                    <TextField
+                      label="Cargar Saldo"
+                      type="number"
+                      value={moneyToCharge}
+                      onChange={e =>
+                        this.setState({ moneyToCharge: e.target.value })
+                      }
+                    />
+                    <Button onClick={this.chargeMoney}>Cargar</Button>
+                  </div>
+                  <div>
+                    <div>Pelis vistas</div>
+                    <div>Lista de peliculas </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="lds-ring">
+                <div />
+                <div />
+                <div />
+                <div />
+              </div>
+            )}
+          </CardContent>
+          {!!user && (
+            <CardActions>
+              <Button onClick={this.goToFriendsPage}>Buscar amigos</Button>
+              <Button onClick={this.acepptModifies}>Aceptar</Button>
+              <Button>Cancelar</Button>
+            </CardActions>
+          )}
+        </Card>
+      </div>
+    );
+  }
+}
+
 export default Profile;
