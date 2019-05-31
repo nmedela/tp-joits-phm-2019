a541e0563b9ce7a8aa76852d8d0af214cc7a1598
nmedela <medela.nicolas@gmail.com>
2019-05-30 21:07:17 -0300

    se sube archivo script no sql

--- backend/no-sql Scritps/scriptMovies.js
+++ backend/no-sql Scritps/scriptMovies.js
@@ -1,5 +1,46 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+CONSULTA 1 :
+//db.movies.find({"screenings.cinemaName": "Cinemark"}).map((pelicula) => ({movie :pelicula.title}))
+//db.movies.find({"screenings.cinemaName": "Cinemark"},{title: 1})
 
-//Query para buscar las peliculas que estan en determinado cine
+db.system.js.save(
+{
+   _id:"peliculasConFuncionesEnSala",
+   value: function(sala){
+       return db.movies.find({ "screenings.cinemaName" : sala },{title: 1}
+       )}
+}) 
 
-db.movies.find({"screenings.cinemaName" : "Jardin Japones"}).pretty()
+db.loadServerScripts();
+peliculasConFuncionesEnSala("Cinemark");
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Consulta 2 :
+
+db.system.js.save(
+{
+   _id:"peliculasDeAnioEnMasDeUnaSala",
+   value: function(anio){
+      const cantidad = db.movies.find({$where: "this.screenings.length > 1 && this.year == " + anio }).count() 
+      return "Hay " + cantidad + " peliculas del año " + anio + " que se reproducen en mas de una sala"
+      }
+}) 
+
+db.loadServerScripts();
+peliculasDeAnioEnMasDeUnaSala(1999);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+CONSULTA 3:
+//db.movies.find({"screenings.date": {$gt: ISODate("2020-01-01")}}).map((pelicula) => ({movie :pelicula.title}))
+
+//db.movies.find({"screenings.date": {$gt: ISODate("2019-01-01")}},{title: 1})
+
+db.system.js.save(
+{
+   _id:"peliculasParaVerAPartirDeFecha",
+   value: function(fecha){
+       return db.movies.find({"screenings.date": {$gt: ISODate(fecha)}},{title: 1})
+}
+}) 
 
